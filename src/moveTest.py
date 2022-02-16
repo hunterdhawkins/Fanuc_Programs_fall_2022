ad6cd58c798c138a092543b1446f88fd824dc746
Zach18.04 <zachattack6089@gmail.com>
2022-02-16 13:17:19 -0800

    Added test program

---
+++ src/moveTest.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import numpy as np
+import controller
+from fanuc_demo.msg import fullCoordinate
+import rospy
+import math
+import time
+
+def main():
+    fanuc = controller.FanucInterface()
+    joints = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+
+    for i in range(1, 4):
+        fanuc.go_to_joint_state(joints)
+        time.sleep(5)
+        fanuc.zeroOut()
+        time.sleep(5)
+
+if __name__ == '__main__':
+    main()
